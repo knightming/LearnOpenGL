77a80200b77351d4ae3bcc06dac5d4d5612dc255
Ming <knightming@qq.com>
2020-05-08 18:12:11 +0800

    .

--- OpenGLESDemo/main.cpp
+++ OpenGLESDemo/main.cpp
@@ -49,3 +49,8 @@ void windowSizeDidChangeCallback(GLFWwindow * window, int width, int height) {
     glad_glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
